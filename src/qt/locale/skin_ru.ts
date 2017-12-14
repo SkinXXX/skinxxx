adac3c9dc0176c68bbf4b17ba844776d13626feb
HashToBeWild <hashtobewild@cryptocurrency.ninja>
2017-12-14 13:49:31 +0200

    Revert ColossusCoinXT copyright notices

--- src/qt/locale/skin_ru.ts
+++ src/qt/locale/skin_ru.ts
@@ -3208,7 +3208,7 @@ https://www.transifex.com/skin-project/skin-project-translations</translation>
     </message>
     <message>
         <source>Copyright (C) 2015-%i The PIVX Core Developers</source>
-        <translation>Copyright (C) 2015-%i Разработчики SKIN Core</translation>
+        <translation>Copyright (C) 2015-%i Разработчики COLX Core</translation>
     </message>
     <message>
         <source>Corrupted block database detected</source>
